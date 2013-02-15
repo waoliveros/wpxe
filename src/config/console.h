3ab0aa0c8595dde906b202e0071cb379df8b9517
Wigi Vei Oliveros <waoliveros@gmail.com>
2013-02-15 16:55:52 +0800

    [console] Enable CONSOLE_SYSLOG and set log level to LOG_ALL

--- src/config/console.h
+++ src/config/console.h
@@ -18,14 +18,14 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #define	CONSOLE_SERIAL		/* Serial port */
 //#define	CONSOLE_DIRECT_VGA	/* Direct access to VGA card */
 //#define	CONSOLE_PC_KBD		/* Direct access to PC keyboard */
-//#define	CONSOLE_SYSLOG		/* Syslog console */
+#define	CONSOLE_SYSLOG		/* Syslog console */
 //#define	CONSOLE_SYSLOGS		/* Encrypted syslog console */
 #define	CONSOLE_VMWARE		/* VMware logfile console */
 //#define	CONSOLE_DEBUGCON	/* Debug port console */
 
 #define	KEYBOARD_MAP	us
 
-#define	LOG_LEVEL	LOG_NONE
+#define	LOG_LEVEL	LOG_ALL
 
 #include <config/local/console.h>
 
